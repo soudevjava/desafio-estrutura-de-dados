c4b03cca08553a65009da6381e8aea8af6bc8344
Emerson de Farias Santos <e.fariassantos@gmail.com>
2025-10-11 01:42:40 -0300

    feat(domain): add custom domain exceptions
    - Create LivroNaoEncontradoException for missing books
    - Create LivroDuplicadoException for duplicate books
    - Add meaningful error messages
    - Extend Exception for checked exception handling

---
+++ src/domain/exceptions/LivroDuplicadoException.java
@@ -0,0 +1,5 @@
+package domain.exceptions;
+
+public class LivroDuplicadoException extends Exception {
+    public LivroDuplicadoException(String message) { super(message); }
+}
---
+++ src/domain/exceptions/LivroNaoEncontradoException.java
@@ -0,0 +1,5 @@
+package domain.exceptions;
+
+public class LivroNaoEncontradoException extends Exception {
+    public LivroNaoEncontradoException(String message) { super(message); }
+}
